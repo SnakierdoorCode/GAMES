e0d6fb6dbb28aea0f4d87da25a841fd8a6a77998
SnakierdoorCode <shg9g2tfht@privaterelay.appleid.com>
2026-05-25 18:21:14 -0400

    Update main.js

--- Ports/Thats-not-my-neighbor/main.js
+++ Ports/Thats-not-my-neighbor/main.js
@@ -40,7 +40,7 @@ function getParts(file, start, end) {
 }
 
 Promise.all([
-    mergeFiles(getParts("notmyneighbor.pck", 1, 20)),
+    mergeFiles(getParts("notmyneighbor.pck", 1, 23)),
     mergeFiles(getParts("notmyneighbor.wasm", 1, 3))
 ]).then(([pckUrl, wasmUrl]) => {
     window.fetch = async function (url, ...args) {
